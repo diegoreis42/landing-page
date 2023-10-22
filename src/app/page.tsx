507d1e0d9489b5b3d2bab30dd9537c0213ba06fa
Bryan Muller <brysn.ms@gmail.com>
2023-10-22 12:15:33 -0300

    Criacao perguntas frequentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
-import { Hero } from '@/app/components/hero'
+import { Hero } from "@/app/components/hero";
+import Faq from "./components/accordion";
 
 export default function Home() {
   return (
     <main>
-      <Hero 
+      <Hero
         mainText="Eius et voluptatem et ut."
-        subText='At quas ab aspernatur et ut.'
+        subText="At quas ab aspernatur et ut."
         image={{
-          path: '/img/team-xl.svg',
-          alt: 'essa eh a logo principal',
-          size: 700
+          path: "/img/team-xl.svg",
+          alt: "essa eh a logo principal",
+          size: 700,
         }}
-        buttonText='Saiba mais'
+        buttonText="Saiba mais"
       />
+
+      <Faq></Faq>
     </main>
-  )
+  );
 }
